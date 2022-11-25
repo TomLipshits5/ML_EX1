70a609feb58b6ab543f408f9770a0213d2a96bc6
llevi1 <lior_levi@intuit.com>
2022-11-25 11:37:16 +0200

    2a, 2e, 2f

--- task2.py
+++ task2.py
@@ -40,8 +40,6 @@ def getTestSample(testSize: int):
 def predictAndCalcError(k: int, x_train: np.array, y_train: np.array, x_test: np.array, y_test: np.array):
     startTime = current_milli_time()
     classifier = nn.learnknn(k, x_train, y_train)
-    print('nn.learnknn: {0}'.format(current_milli_time() - startTime))
-    startTime = current_milli_time()
     preds = nn.predictknn(classifier, x_test)
     print('nn.predictknn: {0}'.format(current_milli_time() - startTime))
     return np.mean(y_test.reshape((y_test.shape[0], 1)) != preds)
@@ -54,13 +52,12 @@ def corruptSample(x_train, y_train):
     return x_train, y_train
 
 
-def runTestCase(x_test: np.array, y_test: np.array, reps: int, corrupt: bool, Xs: list, Ks: list,
-                sampleSizes: list, title: str, xlable: str, ylable: str):
+def runTestCase(x_test: np.array, y_test: np.array, Xs: list, Ks: list, sampleSizes: list,
+                reps=10, corrupt=False, title='title', xlable='Xs', ylable='Ys'):
     errors = [np.zeros(10, float) for _ in Xs]
     for idx, sampleSize in enumerate(Xs):
         print('idx: {}'.format(idx))
         for i in range(reps):
-            print('i: {}'.format(i))
             x_train, y_train = getTrainSample(sampleSizes[idx])
             if corrupt:
                 x_train, y_train = corruptSample(x_train, y_train)
@@ -80,23 +77,24 @@ def task2a():
     sampleSizes = [i*10 for i in range(1, 11)]
     Ks = [1 for _ in sampleSizes]
     x_test, y_test = getTestSample(len(test2) + len(test3) + len(test5) + len(test6))
-    runTestCase(x_test, y_test, 10, False, sampleSizes, Ks, sampleSizes, "Task 2a", "train sample size", "error")
+    runTestCase(x_test, y_test, sampleSizes, Ks, sampleSizes,
+                title="Task 2a", xlable="train sample size", ylable="error")
 
 
 def task2e():
     Ks = np.arange(1, 12)
     sampleSizes = [200 for _ in Ks]
     x_test, y_test = getTestSample(len(test2) + len(test3) + len(test5) + len(test6))
-    # '''
-    runTestCase(x_test, y_test, 10, False, Ks, Ks, sampleSizes, "Task 2e", "K value", "error")
+    runTestCase(x_test, y_test, Ks, Ks, sampleSizes,
+                title="Task 2e", xlable="K value", ylable="error")
 
 
 def task2f():
     Ks = np.arange(1, 12)
     sampleSizes = [200 for _ in Ks]
     x_test, y_test = getTestSample(len(test2) + len(test3) + len(test5) + len(test6))
-    # '''
-    runTestCase(x_test, y_test, 10, True, Ks, Ks, sampleSizes, "Task 2f", "K value", "error")
+    runTestCase(x_test, y_test, Ks, Ks, sampleSizes,
+                corrupt=True, title="Task 2f", xlable="K value", ylable="error")
 
 
 if __name__ == '__main__':
